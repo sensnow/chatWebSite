0a37a7556f234ada9ea7dcd60a4948df288fb7a5
zhujunlin <1031187482@qq.com>
2023-04-04 14:59:05 +0800

    2.完成基本组件

---
+++ src/components/ChatBubble.vue
@@ -0,0 +1,40 @@
+<template>
+    <div :class="bubbleClass">
+        <p>{{ message.text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        message: {
+            type: Object,
+            required: true,
+        },
+        fromUser: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    computed: {
+        bubbleClass() {
+            return this.fromUser ? 'user-bubble' : 'bot-bubble';
+        },
+    },
+}
+</script>
+
+<style scoped>
+.user-bubble {
+    background-color: #2351e3;
+    color: white;
+    text-align: right;
+}
+
+.bot-bubble {
+    background-color: #f8f9fc;
+    color: #3a3b45;
+    text-align: left;
+}
+
+</style>
